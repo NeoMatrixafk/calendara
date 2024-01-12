c3786c289787504d2644558ed744eeb59b973f24
AyushSharma-pdf <ayusharmamta@gmail.com>
2024-01-13 00:41:06 +0530

    Modified calendar

--- frontend/src/Components/Calendar/Popping.jsx
+++ frontend/src/Components/Calendar/Popping.jsx
@@ -1,58 +1,106 @@
 import { Modal, Button } from "react-bootstrap";
-import React, { useState } from "react";
-import "../../style/model.scss"
+import React from "react";
 import { connect } from "react-redux";
-import {Link} from "react-router-dom"
-import { deleteEventApi, ShowEventsApi, closeEvent } from "../../Redux/actions";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { deleteEventApi, closeEvent } from "../../Redux/actions";
 
-const Popping = ({open, handleClose, event, deleteEventApi, renderStatus, rerender})=> {
-   const navigate = useNavigate();
-   const {id, describe, title, start, end} = event;
+const Popping = ({
+    open,
+    handleClose,
+    event,
+    deleteEventApi,
+    renderStatus,
+    rerender,
+    mode,
+}) => {
+    const { id, describe, title, start, end } = event;
 
-   const handleDelete =async () => {
-     await deleteEventApi(event.id);
-     rerender(!renderStatus)
-   }
+    const handleDelete = async () => {
+        await deleteEventApi(event.id);
+        rerender(!renderStatus);
+    };
 
-   
+    const modal = () => {
+        return (
+            <Modal show={open} onHide={handleClose}>
+                <Modal.Header
+                    closeButton
+                    closeVariant={mode === "light" ? "black" : "white"}
+                    style={{
+                        backgroundColor: mode === "light" ? "" : "#36393e",
+                    }}
+                >
+                    <Modal.Title
+                        className={`text-capitalize text-${
+                            mode === "light" ? "black" : "white"
+                        }`}
+                    >
+                        {title}
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body
+                    style={{
+                        backgroundColor: mode === "light" ? "" : "#36393e",
+                    }}
+                >
+                    {describe ? (
+                        <p
+                            className={`lead text-${
+                                mode === "light" ? "black" : "white"
+                            }`}
+                        >
+                            {describe}
+                        </p>
+                    ) : (
+                        "No Descriptions Yet"
+                    )}
+                    <div
+                        className={`row justify-content-between text-${
+                            mode === "light" ? "black" : "white"
+                        }`}
+                    >
+                        <p className="col small text-center pb-0 mb-0">
+                            from: {start}
+                        </p>
+                        <p className="col small text-center pb-0 mb-0">
+                            to: {end}
+                        </p>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer
+                    style={{
+                        backgroundColor: mode === "light" ? "" : "#36393e",
+                    }}
+                    className="justify-content-center"
+                >
+                    <Button variant="warning" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Link to={`/event/${id}/update`}>
+                        <Button variant="success">Update</Button>
+                    </Link>
+                    <Button variant="danger" onClick={handleDelete}>
+                        Delete
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        );
+    };
 
-   const modal = ()=>{
-     return (
-      <Modal show={open} onHide={handleClose}>
-        <Modal.Header closeButton>
-            <Modal.Title className="text-capitalize">{title}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            {describe? <p className="lead">{describe}</p>: "No Dsecriptions Yet"}
-            <div className="row justify-content-between">
-              <p className="col small text-muted text-center pb-0 mb-0">from: {start}</p>
-              <p className="col small text-muted text-center pb-0 mb-0">to: {end}</p>
-            </div>
-          </Modal.Body>
-          <Modal.Footer>
-     
-            <Button variant="warning" onClick={handleClose}>Close</Button>
-            <Link to={`/event/${id}/update`}><Button variant="success">Update</Button></Link>
-            <Button variant="danger" onClick={handleDelete}>Delete</Button>
-        </Modal.Footer>
-      </Modal>
-     )
-   }
+    if (id) {
+        return modal();
+    } else {
+        <p>there is no modal to preview</p>;
+    }
+};
 
-   if(id){
-     return modal()
-   }else{
-     <p>there is no modal to preview</p>
-   }
-   
-  }
+function mapStateToProps({ event }) {
+    return {
+        event,
+        //  modalStatus
+    };
+}
 
-  function mapStateToProps({event}){
-     return {
-       event,
-      //  modalStatus
-     }
-  }
-  
-  export default connect(mapStateToProps, {deleteEventApi, closeEvent})(Popping)
+export default connect(mapStateToProps, { deleteEventApi, closeEvent })(
+    Popping
+);
