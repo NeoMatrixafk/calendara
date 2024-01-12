38570d272283b3d60cf4ec91d70514ce18b9e71e
AyushSharma-pdf <ayusharmamta@gmail.com>
2024-01-13 00:59:51 +0530

    Modified AddEvents page

--- frontend/src/Components/Calendar/ColorPalette.jsx
+++ frontend/src/Components/Calendar/ColorPalette.jsx
@@ -1,25 +1,38 @@
 import React, { useState } from "react";
-import { GithubPicker } from "react-color";
+import { TwitterPicker } from "react-color";
 
 const ColorPalette = ({ onSelectColor, initialColor }) => {
-  const [color, setColor] = useState(initialColor || "#3174ad"); // Default color
+    const [color, setColor] = useState(initialColor || "#3174ad"); // Default color
 
-  const handleChange = (selectedColor) => {
-    setColor(selectedColor.hex);
-  };
+    const handleChange = (selectedColor) => {
+        setColor(selectedColor.hex);
+    };
 
-  const handleSelect = (e) => {
-    e.preventDefault(); // Prevent default form submission behavior
-    onSelectColor(color);
-    alert(`Color selected: ${color}`);
-  };
+    const handleSelect = (e) => {
+        e.preventDefault(); // Prevent default form submission behavior
+        onSelectColor(color);
+        alert(`Color selected: ${color}`);
+    };
 
-  return (
-    <div>
-      <GithubPicker color={color} onChange={handleChange} />
-      <button className="btn" onClick={handleSelect} style={{backgroundColor: color}}>Select Color</button>
-    </div>
-  );
+    return (
+        <div>
+            <div>
+                <TwitterPicker color={color} onChange={handleChange} />
+            </div>
+            <div
+                style={{ width: "51%" }}
+                className="d-flex justify-content-center"
+            >
+                <button
+                    className="btn mt-3"
+                    onClick={handleSelect}
+                    style={{ backgroundColor: color }}
+                >
+                    Select Color
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default ColorPalette;
