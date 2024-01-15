2edc98f9e0291283202beff001c1a4a45c2a29d9
AyushSharma-pdf <ayusharmamta@gmail.com>
2024-01-15 13:15:10 +0530

    Removed typo

--- frontend/src/Components/Login/ErrorToast.js
+++ frontend/src/Components/Login/ErrorToast.js
@@ -3,43 +3,48 @@ import { Toast } from "react-bootstrap";
 
 const ErrorToast = (props) => {
     return (
-        <Toast
-            show={props.showToast}
-            onClose={props.toggleToast}
-            className="position-fixed bottom-0 end-0 m-3"
-            style={{
-                position: "fixed",
-                zIndex: "9999",
-                overflow: "auto",
-            }}
-        >
-            <Toast.Header
-                style={{
-                    backgroundColor: props.mode === "light" ? "" : "#474b52",
-                }}
-                className={`border-secondary${
-                    props.mode === "light" ? "-subtle" : ""
-                }`}
-            >
-                <strong
-                    className={`me-auto text-${
-                        props.mode === "light" ? "black" : "white"
-                    }`}
+        <>
+            <div style={{ position: "relative", zIndex: 1 }}>
+                <Toast
+                    show={props.showToast}
+                    onClose={props.toggleToast}
+                    className="position-fixed bottom-0 end-0 m-3"
+                    style={{
+                        zIndex: 9999,
+                        overflow: "auto",
+                    }}
                 >
-                    Login Error
-                </strong>
-            </Toast.Header>
-            <Toast.Body
-                style={{
-                    backgroundColor: props.mode === "light" ? "" : "#474b52",
-                }}
-                className={`me- text-${
-                    props.mode === "light" ? "black" : "white"
-                }`}
-            >
-                This feature will be added shortly.
-            </Toast.Body>
-        </Toast>
+                    <Toast.Header
+                        style={{
+                            backgroundColor:
+                                props.mode === "light" ? "" : "#474b52",
+                        }}
+                        className={`border-secondary ${
+                            props.mode === "light" ? "-subtle" : ""
+                        }`}
+                    >
+                        <strong
+                            className={`me-auto text-${
+                                props.mode === "light" ? "black" : "white"
+                            }`}
+                        >
+                            Login Error
+                        </strong>
+                    </Toast.Header>
+                    <Toast.Body
+                        style={{
+                            backgroundColor:
+                                props.mode === "light" ? "" : "#474b52",
+                        }}
+                        className={`me- text-${
+                            props.mode === "light" ? "black" : "white"
+                        }`}
+                    >
+                        This feature will be added shortly.
+                    </Toast.Body>
+                </Toast>
+            </div>
+        </>
     );
 };
 
