56282c4120c661537d245189e68c19800a571bd8
AyushSharma-pdf <ayusharmamta@gmail.com>
2024-01-17 22:37:52 +0530

    Added ScrollToTop feature

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,29 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-import store, {persistor} from "./Redux/store"
-import { PersistGate } from 'redux-persist/integration/react';
+import store, { persistor } from "./Redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import ScrollToTop from "./ScrollToTop";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  
-<React.StrictMode>
-
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-
-          <App />
-
-        </BrowserRouter>
-    </PersistGate>
-  </Provider>
-
-</React.StrictMode>
-
+    <React.StrictMode>
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <BrowserRouter>
+                    <ScrollToTop />
+                    <App />
+                </BrowserRouter>
+            </PersistGate>
+        </Provider>
+    </React.StrictMode>
 );
