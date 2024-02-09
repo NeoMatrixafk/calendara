74590cd54077de6bcf3f737dec9c57522d867519
AustinK <125139752+NeoMatrixafk@users.noreply.github.com>
2024-02-09 11:20:44 +0530

    Within 7, 3, 1 day(s) reminders

--- frontend/src/Pages/Reminders.js
+++ frontend/src/Pages/Reminders.js
@@ -2,9 +2,25 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Reminders = (props) => {
+    const [events7days, setEvents7days] = useState([]);
     const [events3days, setEvents3days] = useState([]);
     const [events1day, setEvents1day] = useState([]);
     const userName = localStorage.getItem("userName");
+
+    useEffect(() => {
+        async function fetch7daysEvents() {
+            try {
+                const response = await axios.get(
+                    `http://localhost:55555/api/reminders/7days/${userName}`
+                );
+                setEvents7days(response.data);
+            } catch (error) {
+                console.error("Error fetching events:", error);
+            }
+        }
+
+        fetch7daysEvents();
+    }, [userName]);
 
     useEffect(() => {
         async function fetch3daysEvents() {
@@ -43,6 +59,25 @@ const Reminders = (props) => {
             }`}
         >
             <h1>Reminders</h1>
+            <div className="container my-5">
+                <h3>Within 7 Days</h3>
+                {events7days.map((event) => (
+                    <li key={event._id}>
+                        <h2>{event.title}</h2>
+                        <p>
+                            <strong>Start:</strong>{" "}
+                            {new Date(event.start).toLocaleString()}
+                        </p>
+                        <p>
+                            <strong>End:</strong>{" "}
+                            {new Date(event.end).toLocaleString()}
+                        </p>
+                        <p>
+                            <strong>Description:</strong> {event.describe}
+                        </p>
+                    </li>
+                ))}
+            </div>
             <div className="container my-5">
                 <h3>Within 3 Days</h3>
                 {events3days.map((event) => (
