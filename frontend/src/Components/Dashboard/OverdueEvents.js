d662a9ed8bcc35cd512489fb5bd0dfd81e6d56bf
AustinK <125139752+NeoMatrixafk@users.noreply.github.com>
2024-02-11 18:02:10 +0530

    Implementing backend to Dashboard

--- frontend/src/Components/Dashboard/OverdueEvents.js
+++ frontend/src/Components/Dashboard/OverdueEvents.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { PieChart, Pie, Tooltip, Cell } from "recharts";
+import axios from "axios";
+
+
 
 const OverdueEvents = (props) => {
+
+    const [totalEventsCount, setTotalEventsCount] = useState(0);
+    const userName = localStorage.getItem("userName");
+
+    useEffect(() => {
+        const fetchTotalEventsData = async () => {
+            try {
+                const response = await axios.get(`http://localhost:55555/api/reminders/total/${userName}`);
+                const events = response.data;
+                setTotalEventsCount(events.length);
+            } catch (error) {
+                console.error("Error fetching upcoming events:", error);
+            }
+        };
+
+        fetchTotalEventsData();
+    }, [userName]);
+
     const data = [
-        { name: "Overdue Events", value: 2400 },
-        { name: "Total Events", value: 4567 },
+        { name: "Overdue Events", value: 1 },
+        { name: "Total Events", value: totalEventsCount },
     ];
 
     const totalEventsColor = props.mode === "light" ? "#e6e6e6" : "#474b52";
