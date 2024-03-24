20fa2e6756f55ce8bc10abbb32a6fe18b7487832
AyushSharma-pdf <ayusharmamta@gmail.com>
2024-03-25 01:29:03 +0530

    Added automated email script

--- backend/routes/sendMail.js
+++ backend/routes/sendMail.js
@@ -2,8 +2,6 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 const router = express.Router();
 
-
-
 const transporter = nodemailer.createTransport({
     service: "gmail",
     host: "smtp.gmail.com",
@@ -99,5 +97,75 @@ router.post("/", async (req, res) => {
         res.status(500).send("Error sending email");
     }
 });
+
+// setInterval(async () => {
+//     try {
+//         const mailOptions = {
+//             from: {
+//                 name: "calendara Support",
+//                 address: process.env.EMAIL_USER,
+//             },
+//             to: ["user@email.com"], // Replace with your recipient email address
+//             subject: "Calendara Reminder Email",
+//             html: `
+//                 <html>
+//                     <head>
+//                     <style>
+//                         .container {
+//                             display: block; /* Change to block */
+//                             margin: 0 auto; /* Center horizontally */
+//                             text-align: center; /* Center text within the container */
+//                         }
+
+//                         p {
+//                             font-size: 18px;
+//                         }
+
+//                         ul {
+//                             list-style-type: none;
+//                             padding: 0;
+//                             font-size: 18px;
+//                         }
+
+//                         li {
+//                             margin-bottom: 8px;
+//                         }
+
+//                         a {
+//                             display: inline-block;
+//                             padding: 10px;
+//                             background-color: #edc9af;
+//                             color: #ffffff;
+//                             text-decoration: none;
+//                             border-radius: 5px;
+//                         }
+//                     </style>
+
+//                     </head>
+//                     <body>
+//                         <div class="container">
+//                             <img src="https://github.com/ayush-sharma11/calendara/blob/master/frontend/public/Images/Logo/calendara_light.png?raw=true" alt="Calendara Logo">
+//                             <p>Hello User,</p>
+//                             <p>This is a reminder about your following upcoming events within 24 hours:</p>
+//                             <ul>
+//                                 <b>
+//                                     Placeholder for eventTitles1Day
+//                                 </b>
+//                             </ul>
+//                             <p>Click on the link below to check your events in calendara</p>
+//                             <a href="https://calendara-65xh.onrender.com/#/events">View Your Events</a>
+//                             <p>Thank you for using calendara!</p>
+//                         </div>
+//                     </body>
+//                 </html>
+//             `,
+//         };
+
+//         await transporter.sendMail(mailOptions);
+//         console.log("Email has been sent");
+//     } catch (error) {
+//         console.error("Error sending email:", error);
+//     }
+// }, 60000); // 60000 milliseconds = 1 minute
 
 module.exports = router;
